6a884176b4581ff2032923e64a110159f7ba3282
ー 灰桜 ー <hhbilly99@gmail.com>
2020-02-18 20:24:19 +0800

    📅 Fixed bug of date calc.

--- runcheck.sh
+++ runcheck.sh
@@ -23,11 +23,12 @@ subject=$(sed -n '5p' ./tmp/${1}.info)
 rm -f ./tmp/ca.info
 rm -f ./tmp/${1}.info
 
-DATE="$(echo $(date '+%Y-%m-%d %H:%M:%S %Z'))"
+DATE="$(echo $(date '+%Y-%m-%d %H:%M:%S'))"
 
 nowstamp="$(date -d "$DATE" +%s)"
 expirestamp="$(date -d "$expire" +%s)"
-expireday=`expr $[expirestamp-nowstamp] / 86400`
+expireday=$((expirestamp-nowstamp))
+expireday=$((expireday/86400))
 
 echo '{' > tmp/${1}.json
 echo '"domain": "'${1}'",' >> ./tmp/${1}.json
@@ -35,23 +36,23 @@ echo '"subject": "'$subject'",' >> ./tmp/${1}.json
 echo '"start": "'$start'",' >> ./tmp/${1}.json
 echo '"expire": "'$expire'",' >> ./tmp/${1}.json
 echo '"issuer": "'$issuer'",' >> ./tmp/${1}.json
+echo '"check": "'$DATE'",' >> ./tmp/${1}.json
+echo '"remain": "'$expireday'",' >> ./tmp/${1}.json
 
 if [ $expirestamp \< $nowstamp ]; then
     echo '"status": "Expired",' >> ./tmp/${1}.json
-    echo '"statuscolor": "error",' >> ./tmp/${1}.json
+    echo '"statuscolor": "error"' >> ./tmp/${1}.json
 elif [ $expireday \< 10 ]; then
     echo '"status": "Soon Expired",' >> ./tmp/${1}.json
-    echo '"statuscolor": "warning",' >> ./tmp/${1}.json
+    echo '"statuscolor": "warning"' >> ./tmp/${1}.json
 elif [ $status = "ok." ]; then
     echo '"status": "Valid",' >> ./tmp/${1}.json
-    echo '"statuscolor": "success",' >> ./tmp/${1}.json
+    echo '"statuscolor": "success"' >> ./tmp/${1}.json
 else
     echo '"status": "Invalid",' >> ./tmp/${1}.json
-    echo '"statuscolor": "error",' >> ./tmp/${1}.json
+    echo '"statuscolor": "error"' >> ./tmp/${1}.json
 fi
 
-echo '"check": "'$DATE'",' >> ./tmp/${1}.json
-echo '"remain": "'$expireday'"' >> ./tmp/${1}.json
 echo '}' >> ./tmp/${1}.json
 
 cp -rf ./tmp/${1}.json ./results/${1}.json
